8cef99e11fc21f5420c9a0c7d9cb40081961c5eb
Luis Martinez <dotWicho@gmail.com>
2020-05-26 17:40:21 -0300

    Update dependencies with Marathon Client implementation

--- marathon.go
+++ marathon.go
@@ -2,6 +2,7 @@ package marathon
 
 import (
 	"errors"
+	"fmt"
 	"github.com/dotWicho/requist"
 	"net/url"
 	"time"
@@ -21,57 +22,15 @@ type client interface {
 	MarathonLeader() string
 	MarathonFramework() string
 	MarathonZookeeper() string
-
-	// Marathon AppDefinition interface
-	AppCreate(app AppDefinition) error
-	AppDestroy(id string) error
-	AppUpdate(app AppDefinition) error
-
-	AppScale(id string, instances int, force bool) error
-	AppStop(id string, force bool) error
-	AppStart(id string, instances int, force bool) error
-	AppRestart(id string, force bool) error
-	AppSuspend(id string, force bool) error
-
-	AppGetTag(id string) (string, error)
-	AppSetTag(id string, tag string, force bool) error
-
-	AppEnv(id string) map[string]string
-	AppSetEnv(id string, name, value string, force bool) error
-	AppDelEnv(id string, name string, force bool) error
-
-	AppCpus(id string) float64
-	AppSetCpus(id string, to float64, force bool) error
-
-	AppMemory(id string) float64
-	AppSetMemory(id string, to float64, force bool) error
-
-	AppRole(id string) string
-	AppSetRole(id string, to string, force bool) error
-
-	AppContainer(id string) *Container
-	AppSetContainer(id string, to *Container, force bool) error
-
-	AppAddParameter(id string, key, value string, force bool) error
-	AppDelParameter(id string, key string, force bool) error
-
-	AppLoadFromFile(fileName string) error
-	AppDumpToFile(id, fileName string) error
 }
 
 // Marathon application implementation
 type Client struct {
-	client  *requist.Requist
+	Session *requist.Requist
 	timeout time.Duration
 
 	//
 	info Info
-
-	//
-	ma *Application
-	mg *Groups
-	md *Deployment
-	mt *Tasks
 
 	//
 	fail FailureMessage
@@ -111,55 +70,45 @@ func NewClientFromURL(base *url.URL) *Client {
 func (mc *Client) New(base *url.URL) *Client {
 
 	marathon := &Client{}
-	marathon.client = requist.New(base.String())
+	marathon.Session = requist.New(base.String())
 	marathon.baseUrl = base.String()
 
 	if base.User.String() != "" {
 		if pass, check := base.User.Password(); check {
-			marathon.client.SetBasicAuth(base.User.Username(), pass)
+			marathon.Session.SetBasicAuth(base.User.Username(), pass)
 		}
-		marathon.auth = marathon.client.GetBasicAuth()
+		marathon.auth = marathon.Session.GetBasicAuth()
 	}
 	marathon.SetTimeout(defaultDeploymentTimeout)
-	marathon.client.Accept("application/json")
-	marathon.client.SetHeader("Cache-Control", "no-cache")
-	marathon.client.SetHeader("Accept-Encoding", "identity")
-
-	marathon.ma = NewMarathonApplication(mc.timeout)
-	marathon.ma.client = marathon.client
-	marathon.ma.auth = marathon.auth
-
-	marathon.mg = nil
-	marathon.md = nil
-	marathon.mt = nil
+	marathon.Session.Accept("application/json")
+	marathon.Session.SetHeader("Cache-Control", "no-cache")
+	marathon.Session.SetHeader("Accept-Encoding", "identity")
 
 	return marathon
 }
 
 // Connect sets baseUrl and prepares the Client with this
 func (mc *Client) Connect(baseUrl string) {
-	mc.client = nil
-	nc := mc.client.New(baseUrl)
-	mc.client = nc
+	mc.Session = nil
+	nc := mc.Session.New(baseUrl)
+	mc.Session = nc
 }
 
 // StatusCode returns last responseCode
 func (mc *Client) StatusCode() int {
-	return mc.client.StatusCode()
+	return mc.Session.StatusCode()
 }
 
 // CheckConnection send a request to check Marathon server connectivity
 func (mc *Client) CheckConnection() error {
 
-	if _, err := mc.client.Get(marathonApiPing, nil, nil); err != nil {
-		return err
+	if _, err := mc.Session.Get(marathonApiPing, nil, nil); err != nil {
+		return errors.New(fmt.Sprintf("unable to connect to Marathon server %s.\n", mc.baseUrl))
 	}
 	if mc.StatusCode() == 200 {
-		if _, err := mc.client.Get(marathonApiInfo, mc.info, mc.fail); err != nil {
-			return err
+		if _, err := mc.Session.Get(marathonApiInfo, mc.info, mc.fail); err != nil {
+			return errors.New(fmt.Sprintf("unable to get info from Marathon server %s.\n", mc.baseUrl))
 		}
-	} else {
-		return errors.New("unable to connect")
 	}
 	return nil
 }
@@ -168,13 +117,13 @@ func (mc *Client) CheckConnection() error {
 func (mc *Client) SetTimeout(timeout time.Duration) {
 
 	mc.timeout = timeout
-	mc.client.SetClientTimeout(mc.timeout)
+	mc.Session.SetClientTimeout(mc.timeout)
 }
 
 // SetBasicAuth used if we need to set login parameters
 func (mc *Client) SetBasicAuth(username, password string) {
 
-	mc.client.SetBasicAuth(username, password)
+	mc.Session.SetBasicAuth(username, password)
 }
 
 //=== Marathon Info interface definitions ===
@@ -201,224 +150,4 @@ func (mc *Client) MarathonFramework() string {
 func (mc *Client) MarathonZookeeper() string {
 
 	return mc.info.ZookeeperConfig.Zk
-}
-
-//=== Marathon AppDefinition interface definitions ===
-
-// Marathon AppCreate calls MarathonApplication.Create
-func (mc *Client) AppCreate(app AppDefinition) error {
-
-	_, err := mc.ma.Create(app)
-
-	return err
-}
-
-// Marathon AppDestroy calls MarathonApplication.Destroy
-func (mc *Client) AppDestroy(id string) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.Destroy()
-}
-
-// Marathon AppUpdate calls MarathonApplication.Update
-func (mc *Client) AppUpdate(app AppDefinition) error {
-
-	return mc.ma.Update(app)
-}
-
-// Marathon AppScale calls MarathonApplication.Scale
-func (mc *Client) AppScale(id string, instances int, force bool) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.Scale(instances, force)
-}
-
-// Marathon AppStop calls MarathonApplication.Stop
-func (mc *Client) AppStop(id string, force bool) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.Stop(force)
-}
-
-// Marathon AppStart calls MarathonApplication.Start
-func (mc *Client) AppStart(id string, instances int, force bool) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.Start(instances, force)
-}
-
-// Marathon AppRestart calls MarathonApplication.Restart
-func (mc *Client) AppRestart(id string, force bool) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.Restart(force)
-}
-
-// Marathon AppSuspend calls MarathonApplication.Suspend
-func (mc *Client) AppSuspend(id string, force bool) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.Suspend(force)
-}
-
-// Marathon AppGetTag calls MarathonApplication.GetTag
-func (mc *Client) AppGetTag(id string) (string, error) {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return "", err
-	}
-	return mc.ma.GetTag()
-}
-
-// Marathon AppSetTag calls MarathonApplication.SetTag
-func (mc *Client) AppSetTag(id string, tag string, force bool) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.SetTag(tag, force)
-}
-
-// Marathon AppEnv calls MarathonApplication.Env
-func (mc *Client) AppEnv(id string) map[string]string {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return nil
-	}
-	return mc.ma.Env()
-}
-
-// Marathon AppSetEnv calls MarathonApplication.SetEnv
-func (mc *Client) AppSetEnv(id, name, value string, force bool) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.SetEnv(name, value, force)
-}
-
-// Marathon AppDelEnv calls MarathonApplication.DelEnv
-func (mc *Client) AppDelEnv(id, name string, force bool) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.DelEnv(name, force)
-}
-
-// Marathon AppCpus calls MarathonApplication.Cpus
-func (mc *Client) AppCpus(id string) float64 {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return 0
-	}
-	return mc.ma.Cpus()
-}
-
-// Marathon AppSetCpus calls MarathonApplication.SetCpus
-func (mc *Client) AppSetCpus(id string, to float64, force bool) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.SetCpus(to, force)
-}
-
-// Marathon AppMemory calls MarathonApplication.Memory
-func (mc *Client) AppMemory(id string) float64 {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return 0
-	}
-	return mc.ma.Memory()
-}
-
-// Marathon AppSetMemory calls MarathonApplication.SetMemory
-func (mc *Client) AppSetMemory(id string, to float64, force bool) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.SetCpus(to, force)
-}
-
-// Marathon AppRole calls MarathonApplication.Role
-func (mc *Client) AppRole(id string) string {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return ""
-	}
-	return mc.ma.Role()
-}
-
-// Marathon AppSetRole calls MarathonApplication.SetRole
-func (mc *Client) AppSetRole(id, to string, force bool) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.SetRole(to, force)
-}
-
-// Marathon AppContainer calls MarathonApplication.Container
-func (mc *Client) AppContainer(id string) *Container {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return nil
-	}
-	return mc.ma.Container()
-}
-
-// Marathon AppSetContainer calls MarathonApplication.SetContainer
-func (mc *Client) AppSetContainer(id string, to *Container, force bool) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.SetContainer(to, force)
-}
-
-// Marathon AppAddParameter calls MarathonApplication.AddParameter
-func (mc *Client) AppAddParameter(id string, key, value string, force bool) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.AddParameter(key, value, force)
-}
-
-// Marathon AppDelParameter calls MarathonApplication.DelParameter
-func (mc *Client) AppDelParameter(id string, key string, force bool) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.DelParameter(key, force)
-}
-
-// Marathon AppLoadFromFile calls MarathonApplication.LoadFromFile
-func (mc *Client) AppLoadFromFile(fileName string) error {
-
-	return mc.ma.LoadFromFile(fileName)
-}
-
-// Marathon AppDumpToFile calls MarathonApplication.DumpToFile
-func (mc *Client) AppDumpToFile(id, fileName string) error {
-
-	if _, err := mc.ma.Get(id); err != nil {
-		return err
-	}
-	return mc.ma.DumpToFile(fileName)
 }
